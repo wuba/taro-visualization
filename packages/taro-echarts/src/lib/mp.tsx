e7742261ef24eae5a0347c085be9f100fa1b1424
Ryan <flyrui316@163.com>
2022-12-16 14:53:32 +0800

    feat: taro-echarts mp & rn ok

--- packages/taro-echarts/src/lib/mp.tsx
+++ packages/taro-echarts/src/lib/mp.tsx
@@ -1,62 +1,68 @@
 import { Canvas, View, ViewProps } from '@tarojs/components';
 import Taro from '@tarojs/taro';
+import * as echarts from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
 import * as React from 'react';
-import { forwardRef, memo, useEffect, useMemo, useState } from 'react';
+import { forwardRef, memo, useEffect, useMemo } from 'react';
 
 import EventTarget from './EventTarget';
 import copyProperties from './copyProperties';
 import WxCanvas from './wx-canvas';
 interface IProps {
-    canvasId?:string;
-    onContextCreate?:any;
+  canvasId?: string;
+  onContextCreate: any;
 }
-function EchartsComponetMP(props:IProps , ref?: any) {
+function EchartsComponetMP(props: IProps, ref?: any) {
   const { canvasId, onContextCreate, ...domProps } = useMemo(() => {
     return { ...props };
   }, [props]);
-    const [canvas, setCanvas] = useState();
-    useEffect(() => {
-        setTimeout(() => {
-          const query = Taro.createSelectorQuery();
-          query
-            .select(`#${props.canvasId ?? 'i-echarts'}`)
-            .node()
-            .exec((res) => {
-              const canvas = res[0].node;
-              Object.defineProperty(canvas, 'style', {
-                get() {
-                  return {
-                    width: this.width + 'px',
-                    height: this.height + 'px'
-                  };
-                }
-              });
-              Object.defineProperty(canvas, 'clientHeight', {
-                get() {
-                  return this.height;
-                }
-              });
-              Object.defineProperty(canvas, 'clientWidth', {
-                get() {
-                  return this.width;
-                }
-              });
-              copyProperties(canvas.constructor.prototype, EventTarget.prototype);
-              setCanvas(canvas);
-              const ctx = canvas.getContext('2d');
-              const chartCanvas = new WxCanvas(ctx, props.canvasId ?? 'i-echarts', true, canvas)
-              props.onContextCreate(chartCanvas)
-            });
-        }, 0);
-      }, []);
-      return <View {...domProps} style={{width: 100, height: 100}}><Canvas
+  useEffect(() => {
+    setTimeout(() => {
+      const query = Taro.createSelectorQuery();
+      query
+        .select(`#${props.canvasId ?? 'i-echarts'}`)
+        .node()
+        .exec((res) => {
+          const canvas = res[0].node;
+          Object.defineProperty(canvas, 'style', {
+            get() {
+              return {
+                width: this.width + 'px',
+                height: this.height + 'px'
+              };
+            }
+          });
+          Object.defineProperty(canvas, 'clientHeight', {
+            get() {
+              return this.height;
+            }
+          });
+          Object.defineProperty(canvas, 'clientWidth', {
+            get() {
+              return this.width;
+            }
+          });
+          copyProperties(canvas.constructor.prototype, EventTarget.prototype);
+          const ctx = canvas.getContext('2d');
+          const chartCanvas = new WxCanvas(ctx, props.canvasId ?? 'i-echarts', true, canvas);
+          // @ts-ignore
+          echarts.setPlatformAPI({ createCanvas: () => chartCanvas });
+          props.onContextCreate(chartCanvas);
+        });
+    }, 0);
+  }, []);
+  return (
+    <View {...domProps}>
+      <Canvas
         canvasId={props.canvasId ?? 'i-echarts'}
         id={props.canvasId ?? 'i-echarts'}
         type="2d"
         width="100%"
         height="100%"
         ref={ref}
-      /></View>
+      />
+    </View>
+  );
 }
 export const Echarts = memo(forwardRef(EchartsComponetMP));
-export const SVGRenderer = {};
+export const SVGRenderer = CanvasRenderer;
--- packages/taro-echarts/src/lib/rn.tsx
+++ packages/taro-echarts/src/lib/rn.tsx
@@ -1,16 +1,22 @@
-import React, { forwardRef, memo, useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { SkiaChart, SvgChart } from 'wrn-echarts';
 export { SVGRenderer } from 'wrn-echarts';
 
 interface IProps {
   RNRenderType?: 'svg' | 'skia';
+  onContextCreate: any;
 }
-function EchartsComponetRN(props: IProps, ref?: any) {
+function EchartsComponetRN(props: IProps) {
+  const chartRef = useRef();
   const component = useMemo(() => {
-    if (props.RNRenderType === 'svg') return <SvgChart ref={ref}></SvgChart>;
-    return <SkiaChart ref={ref}></SkiaChart>;
-  }, [props, ref]);
+    if (props.RNRenderType === 'svg') return <SvgChart ref={chartRef}></SvgChart>;
+    return <SkiaChart ref={chartRef}></SkiaChart>;
+  }, [props]);
+
+  useEffect(() => {
+    chartRef.current && props.onContextCreate(chartRef.current);
+  }, [chartRef]);
 
   return component;
 }
-export const Echarts = memo(forwardRef(EchartsComponetRN));
+export const Echarts = EchartsComponetRN;
