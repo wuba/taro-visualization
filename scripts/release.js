80154997495c555af68aade55555bd33f73a028c
Ryan <flyrui316@163.com>
2023-04-04 18:30:07 +0800

    chore: release.js

--- scripts/release.js
+++ scripts/release.js
@@ -46,8 +46,7 @@ const askQuestions = () => {
 
 const buildPackage = ({ packageName }) => {
   const packagePath = path.join(cwd, 'packages', packageName);
-  shell.cd(`${packagePath}`);
-  shell.exec(`rm -rf build`, { silent: true });
+  shell.exec(`rm -rf ${packagePath}/build`, { silent: true });
   console.log(chalk.green(`${packageName}编译中，请耐心等待...`));
   const buildRes = shell.exec(`pnpm -F ${packageName} run build`, { silent: true });
   if (buildRes.code !== 0) {
@@ -59,26 +58,25 @@ const buildPackage = ({ packageName }) => {
 
 const publishPackage = async ({ packageName }) => {
   const pkgJson = await readJson(path.join(cwd, 'packages', packageName, 'package.json'));
-  /** ================ npm 发包 =================== */
-  console.log(chalk.green(`${packageName}@${pkgJson.version} ~~npm~~ 包发布中，请耐心等待...`));
-  //    const npmPublishRes = shell.exec(`pnpm -F ${packageName} publish`,{silent:true})
+  console.log(chalk.green(`${packageName}@${pkgJson.version} 发布中，请耐心等待...`));
   const npmPublishRes = shell.exec(`pnpm -F ${packageName} publish`);
-
-  //    if(npmPublishRes.code !== 0 || npmPublishRes.stdout !== `+ ${packageName}@${version}\n`){
-  //      console.log(npmPublishRes.stderr, chalk.red(`${packageName}@${version} ~~npm~~ 包发布失败，请联系管理员!`))
-  //      console.log(chalk.red(`npm发布指令: pnpm -F ${packageName} publish`))
-  //      shell.exit(1)
-  //    } else {
-  //      console.log(chalk.green(`${packageName}@${version} ~~npm~~ 包发布成功 !`))
-  //    }
+  if (npmPublishRes.code !== 0 || npmPublishRes.stdout !== `+ ${packageName}@${pkgJson.version}\n`) {
+    console.log(npmPublishRes.stderr, chalk.red(`${packageName}@${pkgJson.version} 发布失败`));
+    shell.exit(1);
+  } else {
+    console.log(chalk.green(`${packageName}@${pkgJson.version} 发布成功 !`));
+  }
 };
 const run = async () => {
-  init();
-  //   checkGit();
-  const answers = await askQuestions();
-  buildPackage(answers);
-  await publishPackage(answers);
-  //   shell.exit(0);
+  try {
+    init();
+    //   checkGit();
+    const answers = await askQuestions();
+    buildPackage(answers);
+    await publishPackage(answers);
+  } catch (error) {
+    console.log(1234, error);
+  }
 };
 
 run();
